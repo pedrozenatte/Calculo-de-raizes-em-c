b5e3251f141c88dcfcca9a261cb42016d6bc890b
PedroZenatte <pedrozenatte@usp.br>
2024-01-26 01:03:25 -0300

    Adicionadas as explicações das funções implementadas nos headers de cada biblioteca, além de informar possíveis problemas advindos da formatação utilizada pelo usuário ao passar a expressão da função a ser calculada.

--- includes/Bisseccao.h
+++ includes/Bisseccao.h
@@ -3,6 +3,9 @@
 
 #include "funcao.h"
 
+/*Esse método leva em conta um intervalo definido e contínuo de uma função, ou seja, é necessário saber
+aproximadamente onde a raíz está, sendo uma raíz por vez. 
+ATENÇÃO: Se no intervalo existir mais de uma raíz, ocorrerá um problema no encontro da raíz*/
 double encontra_raiz(tipo_funcao *F);
 
 
--- includes/funcao.h
+++ includes/funcao.h
@@ -7,9 +7,13 @@
 typedef struct funcao tipo_funcao;
 
 //Funções
-tipo_funcao *criar_funcao();
-void destroi_funcao(tipo_funcao **F);
-void guarda_funcao(tipo_funcao *F);
+tipo_funcao *criar_funcao(); //Cria a função 
+void destroi_funcao(tipo_funcao **F); //Deleta a função
+void guarda_funcao(tipo_funcao *F); //Permite que o usuária forneça as informações da função
+/*A função acima (guarda_funcao(tipo_funcao *F)), apenas funcionará como deve caso a expressão matemática esteja
+passada na seguinte forma: AX^y + BX^y-1 + CX^y-2... + Constante. CUIDADO COM CASOS COMO: X^3 + X + 4, por
+exemplo, uma vez que o coeficiente B equivale a ZERO.
+Além disso, o intervalo a ser passado precisa estar em ordem crescente, isto é, do menor para o maior*/
 void imprime_informacoes(const tipo_funcao *F);
 
 #endif // FUNCAO
